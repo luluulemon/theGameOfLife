f3c71bc3238fbf23b76c67511a384d0e8f158cd4
luluulemon <chen_luwei@yahoo.com.sg>
2022-06-22 16:20:02 +0800

    update App.main error msg

--- src/main/java/vtp2022/revision/workshop/App.java
+++ src/main/java/vtp2022/revision/workshop/App.java
@@ -10,12 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {   
+        if (args.length == 0){System.out.println("Please provide path for start game file");}
+
+        else{
         String startGameFilePath = args[0];
         game newGame = new game();
         List<String> data = newGame.readFileMain(startGameFilePath);
         System.out.println("game is of size " + newGame.sizeX + ","+ newGame.sizeY);
         System.out.println("game starts at " + newGame.startX + "," + newGame.startY);
 
-        newGame.gameCycles(data, 5);
+        newGame.gameCycles(data, 5);    }
     }
 }
